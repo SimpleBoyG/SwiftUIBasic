860b5fe8c8b40b2937aa597fa3c817be7a8d710a
정준상 <jjseigneer@gmail.com>
2023-01-26 22:59:07 +0900

    Shape Basic

--- SwiftUIBasic/ShapeBasic.swift
+++ SwiftUIBasic/ShapeBasic.swift
@@ -9,7 +9,52 @@ import SwiftUI
 
 struct ShapeBasic: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20){
+            //circle 원형
+            Text("원형")
+                .font(.title)
+            Circle()
+                .fill(Color.blue) // 좀 더 위에 칠해짐
+                //.foregroundColor(.cyan)
+                //.stroke(Color.red, lineWidth:20)
+                //stroke(Color.purple, style: StrokeStyle(lineWidth: 30, lineCap: .butt, dash: [30]))
+                //.trim(from: 0.2, to: 1.0)
+                //.stroke(Color.purple, lineWidth: 50)
+                .frame(width:300, height:200) // 크기 조절
+                .padding()
+            
+            // Ellipse
+            Text("타원형")
+                .font(.title)
+            Ellipse()
+                .fill(Color.green)
+                .frame(width:200, height:100)
+            
+            // 캡슐형
+            Text("타원형")
+                .font(.title)
+            Ellipse()
+                .fill(Color.green)
+                .frame(width:200, height: 100, alignment: .center)
+        }
+        VStack(spacing: 20){
+            // 직사각형
+            Text("직사각형")
+                .font(.title)
+                .padding(10)
+            Rectangle()
+                .fill(Color.red)
+                .frame(width:200, height: 100, alignment: .center)
+                .padding(10)
+            // 둥근직사각형
+            Text("둥근직사각형")
+                .font(.title3)
+                .bold()
+            RoundedRectangle(cornerRadius: 20)
+                .trim(from: 0.4, to: 1.0)
+                .frame(width: 300, height: 200, alignment: .center)
+            
+        }
     }
 }
 
